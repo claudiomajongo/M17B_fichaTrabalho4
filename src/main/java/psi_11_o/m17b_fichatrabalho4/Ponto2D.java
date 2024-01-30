7ee2be8340a48ce6d7fe4e5bd67a798660dab5c6
Cláudio Majongo <>
2024-01-30 12:15:21 +0000

    Atualização dos repositórios.

--- src/main/java/psi_11_o/m17b_fichatrabalho4/Ponto2D.java
+++ src/main/java/psi_11_o/m17b_fichatrabalho4/Ponto2D.java
@@ -1,10 +1,5 @@
 package psi_11_o.m17b_fichatrabalho4;
 
-/**
- * 
- * @author Cláudio Majongo
- */
-
 public class Ponto2D {
     private int x;
     private int y;
@@ -20,40 +15,14 @@ public class Ponto2D {
     }
     
     public double distancia(Ponto2D a){
-        return Math.abs(Math.sqrt(Math.pow((getX()-a.getX()),2)+Math.pow((getY()-a.getY()),2)));
+        return Math.abs(Math.sqrt(Math.pow((x-a.x),2)+Math.pow((y-a.y),2)));
     }
 
     @Override
     public String toString() {
-        return "Ponto2D[" + getX() + "," + getY() + "]";
+        return "Ponto2D[" + x + "," + y + "]";
     } 
-
-    /**
-     * @return the x
-     */
-    public int getX() {
-        return x;
-    }
-
-    /**
-     * @param x the x to set
-     */
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    /**
-     * @return the y
-     */
-    public int getY() {
-        return y;
-    }
-
-    /**
-     * @param y the y to set
-     */
-    public void setY(int y) {
-        this.y = y;
-    }
+    
+    
+    
 }
-
